461f4e794473fa060e93427c4845d3925ff0a797
mjwynyard <mjwynyard@gmail.com>
2018-03-28 10:25:56 +1300

    All data now in only leaf nodes. Can take juno.dat as input

--- KDTree.py
+++ KDTree.py
@@ -6,31 +6,34 @@ import csv, os
 
 def main():
 
-##    root = Tkinter.Tk()
-##    root.withdraw()
-##    path = tkFileDialog.askopenfilename(parent=root)
+    root = Tkinter.Tk()
+    root.withdraw()
+    path = tkFileDialog.askopenfilename(parent=root)
+    root.destroy() #not working
     
     #Point = namedtuple('Point', ['x', 'y'], verbose=False)
-    points = [(1.5,9.2), (2.7,3.6), (4.2,1.3), (3.7,7.1), (5.9,4.0), (6.2,8.1), (7.4,2.4), (8.8,8.8), (7.1,9.9), (9.5,6.5)]
-    #print median_index(points, 0)
-##    points = []
-##    with open(path, 'r') as f:
-##        reader = csv.reader(f, delimiter=',')
-##        next(f) #skip header
-##        for line in reader:
-##            #print line[0].split('\t')
-##            x = float(line[0].split('\t')[7])
-##            y = float(line[0].split('\t')[6])
-##            z = float(line[0].split('\t')[5])
-##            #print str(x) + " " + str(y) + " " + str(z)
-##            p = x, y, z
-##            #print p.x
-##            points.append(p)
-    k = len(points[0])
+##    pointsF = [(1.5,9.2), (2.7,3.6), (4.2,1.3), (3.7,7.1), (5.9,4.0), (6.2,8.1), (7.4,2.4), (8.8,8.8), (7.1,9.9), (9.5,6.5)]
+##    pointsI = [(1, 9), (2, 3), (4, 1), (3, 7), (5, 4), (6, 8), (7, 2), (8, 8), (7, 9), (9, 6)]
+##    pointsA = [(7, 2), (9, 6), (5, 4), (2, 3), (4, 7), (8, 1)]
+
+    points = []
+    with open(path, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        next(f) #skip header
+        for line in reader:
+            #print line[0].split('\t')
+            x = float(line[0].split('\t')[7])
+            y = float(line[0].split('\t')[6])
+            z = float(line[0].split('\t')[5])
+            #print str(x) + " " + str(y) + " " + str(z)
+            p = x, y, z
+            #print p.x
+            points.append(p)
+    k = len(points[0]) - 1
     #print k
     kd = KDTree()
     kd.build_tree(points, 0, k)
-    print kd.inorder(kd.root)
+    #print kd.inorder(kd.root)
     print kd.getLeafCount(kd.root)
 
 def median_index(points, axis):
@@ -39,8 +42,10 @@ def median_index(points, axis):
 
 class Node:
 
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, value=None):
+        if value is not None:
+            self.value = value
+        self.value = None
         self.left = None
         self.right = None
         self.depth = None
@@ -57,15 +62,23 @@ class KDTree:
     def build_tree(self, points, depth, k):
         axis = depth % k
         i = median_index(points, axis)
-        print points
-        if depth == 2:
-            return
-        node = Node(points[i][axis])
-        #print node.value
-        if self.root == None:       
+        #print points
+
+        node = Node()
+        if self.root == None:
             self.root = node
-        node.left = self.build_tree(points[:i], depth + 1, k)      
-        node.right = self.build_tree(points[i:], depth + 1, k)
+ #           node.points = points
+            node.value = points[i][axis]
+        if len(points) <= 16:
+            node = Node()
+            node.points = points
+ #           node.value = points[i][axis]
+            return node
+        else:
+ #           node.points = points
+            node.value = points[i][axis]
+            node.left = self.build_tree(points[:i], depth + 1, k)
+            node.right = self.build_tree(points[i:], depth + 1, k)
     
         return node
            
@@ -74,6 +87,7 @@ class KDTree:
             return
         else:
             self.inorder(node.left)
+            print node.points
             print node.value
             self.inorder(node.right)
 
